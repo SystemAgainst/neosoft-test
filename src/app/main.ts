96c36884b84a0f122d9658577131656c4350f3e5
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2025-06-18 10:05:22 +1000

    feat: add catalog app and code brush

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,8 +1,8 @@
-import './assets/main.css'
+import '@/app/assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router.ts'
 
 const app = createApp(App)
 
