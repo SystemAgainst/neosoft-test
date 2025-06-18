f6b0404cc04a225d62124e19f1b62e970a621c23
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2025-06-18 10:12:16 +1000

    feat: add tasksApi emulation and task interface

---
+++ src/entities/task/api/tasksApi.ts
@@ -0,0 +1,34 @@
+import type { Task } from '@/shared/types/task';
+
+
+let tasks: Task[] = [
+  { id: 1, title: 'Первое дело', completed: false },
+  { id: 2, title: 'Второе дело', completed: true }
+];
+
+const delay = (ms = 1000) => new Promise(res => setTimeout(res, ms));
+
+export const tasksApi = {
+  async getAll() {
+    await delay();
+    return [...tasks];
+  },
+  async add(title: string) {
+    await delay();
+    const task = { id: Date.now(), title, completed: false };
+    tasks.push(task);
+    return task;
+  },
+  async remove(id: number) {
+    await delay();
+    tasks = tasks.filter(t => t.id !== id);
+  },
+  async toggle(id: number) {
+    await delay();
+    const task = tasks.find(t => t.id === id);
+
+    if (task) task.completed = !task.completed;
+
+    return task;
+  },
+};
